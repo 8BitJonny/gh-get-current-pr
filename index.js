33a77a884cef6515cf239940e135dc1ef8f57e2a
8BitJonny <timon99@gmx.de>
2021-09-16 22:34:09 +0200

    testFix 16: timeout and echo sha

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ async function main() {
 
     let pr = result.data.length > 0 && result.data[0];
 
+    let sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    await sleep(3000);
+
+    const result2 = await new GitHub(token, {}).repos.listPullRequestsAssociatedWithCommit({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        commit_sha: sha || context.sha,
+    });
+    let pr2 = result2.data.length > 0 && result2.data[0];
+
     if (filterOutClosed === true) {
         pr = pr.state === 'open' && pr
     }
@@ -40,6 +50,10 @@ async function main() {
     setOutput('pr_created_at', pr ? pr.created_at : '');
     setOutput('pr_merged_at', pr ? pr.merged_at : '');
     setOutput('pr_closed_at', pr ? pr.closed_at : '');
+
+    setOutput('sha', sha || '');
+    setOutput('context_sha', context.sha || '');
+    setOutput('number2', pr2 && pr2.number.toString() || '');
 }
 
 main().catch(err => setFailed(err.message));
