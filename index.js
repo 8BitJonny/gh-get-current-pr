297884597bf1d651212aae1005d81b9ea30adb89
8BitJonny <timon99@gmx.de>
2021-09-17 10:28:19 +0200

    testFix 16: echo gitLog and commit sha

--- index.js
+++ index.js
@@ -40,6 +40,9 @@ async function main() {
     setOutput('pr_created_at', pr ? pr.created_at : '');
     setOutput('pr_merged_at', pr ? pr.merged_at : '');
     setOutput('pr_closed_at', pr ? pr.closed_at : '');
+
+    setOutput('sha', sha || '');
+    setOutput('context_sha', context.sha || '');
 }
 
 main().catch(err => setFailed(err.message));
