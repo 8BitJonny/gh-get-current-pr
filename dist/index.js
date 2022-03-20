0f8671badbf9d0749f0ae384f63b107567979811
8BitJonny <timon99@gmx.de>
2022-03-20 03:01:26 +0100

    :package: upload dist files

--- dist/index.js
+++ dist/index.js
@@ -186,21 +186,37 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(2186));
-function setOutputWithDebug(key, value) {
-    core.debug(`Setting output: key: "${key}", value: "${value}"`);
-    core.setOutput(key, value);
+function setIfNotNull(map, key, value) {
+    value && map.set(key, value);
 }
+const outputKeys = [
+    'number',
+    'pr',
+    'pr_title',
+    'pr_body',
+    'pr_url',
+    'pr_created_at',
+    'pr_merged_at',
+    'pr_closed_at',
+    'pr_labels'
+];
 function setOutput(pr) {
+    const outputVars = new Map();
     if (pr) {
-        setOutputWithDebug('number', pr.number.toString());
-        setOutputWithDebug('pr', JSON.stringify(pr));
-        setOutputWithDebug('pr_title', pr.title);
-        setOutputWithDebug('pr_body', pr.body);
-        setOutputWithDebug('pr_url', pr.html_url);
-        setOutputWithDebug('pr_created_at', pr.created_at);
-        setOutputWithDebug('pr_merged_at', pr.merged_at);
-        setOutputWithDebug('pr_closed_at', pr.closed_at);
-        setOutputWithDebug('pr_labels', pr.labels.map(e => e.name).join(','));
+        setIfNotNull(outputVars, 'number', pr.number.toString());
+        setIfNotNull(outputVars, 'pr', JSON.stringify(pr));
+        setIfNotNull(outputVars, 'pr_title', pr.title);
+        setIfNotNull(outputVars, 'pr_body', pr.body);
+        setIfNotNull(outputVars, 'pr_url', pr.html_url);
+        setIfNotNull(outputVars, 'pr_created_at', pr.created_at);
+        setIfNotNull(outputVars, 'pr_merged_at', pr.merged_at);
+        setIfNotNull(outputVars, 'pr_closed_at', pr.closed_at);
+        setIfNotNull(outputVars, 'pr_labels', pr.labels.map(e => e.name).join(','));
+    }
+    for (const key of outputKeys) {
+        const value = outputVars.get(key) || '';
+        core.debug(`Setting output: key: "${key}", value: "${value}"`);
+        core.setOutput(key, value);
     }
 }
 exports.default = setOutput;
