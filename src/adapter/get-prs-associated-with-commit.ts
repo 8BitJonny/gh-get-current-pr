e8caa0060fc5dc690db3f678d69585319a50571f
8BitJonny <christiansen.timon@gmail.com>
2022-08-17 12:18:16 +0200

    :loud_sound: add debug output for github api fetchUrl

--- src/adapter/get-prs-associated-with-commit.ts
+++ src/adapter/get-prs-associated-with-commit.ts
@@ -1,4 +1,5 @@
 import * as github from '@actions/github'
+import * as core from '@actions/core'
 import {GitHub} from '@actions/github/lib/utils'
 import {PR} from '../types/pull-request'
 
@@ -11,5 +12,6 @@ export default async function getPullRequestsAssociatedWithCommits(
     repo: github.context.repo.repo,
     commit_sha: sha
   })
+  core.debug(`Used url to fetch associated PRs: ${result.url}`)
   return result.data
 }
